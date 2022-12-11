5342ed9a66a9823d02c0f4fc46289257458c51d8
ObrynKLukongajnr <lukonga.obryn@gmail.com>
2022-12-11 15:19:46 -0800

    Final edit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,10 @@ int main (void)
 char low, e, q;
 e = 'e';
 q = 'q';
-for (low = 'a'; low <= 'z'; low++)
+for(low = 'a'; low <= 'z'; low++)
 {
-if (low != e && low != q)
-{
+if(low != e && low != q)
 putchar(low);
-}
 }
 putchar('\n');
 return (0);
