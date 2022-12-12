3c7b248bc1a85df8985c93f82928a5c5bbf4fd3f
ObrynKLukongajnr <lukonga.obryn@gmail.com>
2022-12-12 00:01:58 -0800

    A task 6 file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry point
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+return (0);
+}
