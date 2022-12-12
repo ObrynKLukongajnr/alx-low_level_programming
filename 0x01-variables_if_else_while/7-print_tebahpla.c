dd9ff7be99e544737d784350d0dac1c393d99939
ObrynKLukongajnr <lukonga.obryn@gmail.com>
2022-12-12 00:08:14 -0800

    A task 7 file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low0;
+				putchar('\n');
+				return (0);
+				}
