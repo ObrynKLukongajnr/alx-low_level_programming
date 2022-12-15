a51e335e3f77244498b40f6496b02ff4e543293d
ObrynKLukongajnr <lukonga.obryn@gmail.com>
2022-12-15 14:34:38 -0800

    task 2 and 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,16 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - print 10 times the alphabet, in lowercase
  */
-
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char l;
-	
-	while (count++ <= 9)
-	{
-		for (l = 'a'; l <= 'z'; l++)
-		_putchar(l);
-	_putchar('\n');
-	}
+int count = 0;
+char l;
+
+while (count++ <= 9)
+{
+	for (l = 'a'; l <= 'z'; l++)
+_putchar(l);
+_putchar('\n');
+}
 }
