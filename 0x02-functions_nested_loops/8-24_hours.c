9878ed208889c53a1b1c8db19571b27122fa359f
ObrynKLukongajnr <lukonga.obryn@gmail.com>
2022-12-27 11:58:54 -0800

    A task 8 file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,7 @@
 #include "main.h"
 
 /*
- * jack_bauer - Check Main
- * Description: function that prints every minute of the day of Jack Bauer,
- * starting from 00:00 to 23:59.
- *
- * Return: Nothing
+ * jack_bauer - prints every minute of the day of
  */
 void jack_bauer(void)
 {
