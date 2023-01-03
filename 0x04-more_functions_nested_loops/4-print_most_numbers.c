d4d44430dd852170baa7398a7d64443ba1d3b3ff
ObrynKLukongajnr <lukonga.obryn@gmail.com>
2023-01-03 15:28:15 -0800

    Task 4 file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_most_numbers - print num except 2 and 4
  *
@@ -11,7 +12,7 @@ void print_most_numbers(void);
 
 	for (c = '0'; c <= '9' c++)
 	{
-		if(!(c == '2' || c == '4'))
+		if (!(c == '2' || c == '4'))
 			_putchar(c);
 	}
 	_putchar('\n');
