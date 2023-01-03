44a32fe6a40081e79ace3b8bdea2925b9b092552
ObrynKLukongajnr <lukonga.obryn@gmail.com>
2023-01-03 07:56:14 -0800

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte.
+ * @s: a string
+ * @n: an integer
+ * Return: Always 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		a[s] = b;
+	return (s);
+}
